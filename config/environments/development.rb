18dd48400a40e8d80aa73c107287f68725110787
Aboobacker MK <aboobackervyd@gmail.com>
2015-07-03 11:30:22 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true,  
   :address        => "smtp.gmail.com",
   :port           => 587,
-  :domain         => "gmail.com",
-  :user_name      => "tachyonscet@gmail.com",
-  :password       => "tachyons999",
+  :domain         => "",
+  :user_name      => "",
+  :password       => "",
   :authentication => :plain
 }
